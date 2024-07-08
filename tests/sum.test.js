afb79d1db3d8ea41ea7a52480926a0de4b078f84
Ian Heraty <heratyian@gmail.com>
2024-07-08 11:37:39 -0500

    Change toBe to toEqual

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -3,11 +3,11 @@ const sum = require("../sum");
 describe('sum', () => {
 
   test('adds 0 + 0 to equal 0', () => {
-    expect(sum(0, 0)).toBe(0);
+    expect(sum(0, 0)).toEqual(0);
   });
 
   test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toEqual(3);
   });
 
 });
